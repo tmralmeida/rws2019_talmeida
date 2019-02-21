b133ef6c84696fc5a89cb051131c1364e81d35c8
Tiago Almeida <tm.almeida@ua.pt>
2019-02-21 10:57:15 +0000

    update

--- player_talmeida/src/player_talmeida.cpp
+++ player_talmeida/src/player_talmeida.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <ros/ros.h>
 
-int main()
+int doSomething()
 {
-    std::cout<< "Hello world"<< std::endl;
-    return 1;
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    // std::cout<< "Hello world"<< std::endl;
+    ros::init(argc, argv, "player_talmeida");
+
+    ros::NoteHandle n;
+    ros::Rate r(1);
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << i << std::endl;
+    }
+
+    return 0;
 }
